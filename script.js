0fe676d57ad17a4270bb3ba764d31d1662d50a2c
raeesf04-code <raeesf04@gmail.com>
2026-06-10 22:21:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-let slides=document.querySelectorAll('.slide');
+// HERO SLIDER
 
-let current=0;
+let slides = document.querySelectorAll('.slide');
+let current = 0;
 
-setInterval(()=>{
+setInterval(() => {
 
 slides[current].classList.remove('active');
 
 current++;
 
-if(current>=slides.length){
-current=0;
+if(current >= slides.length){
+current = 0;
 }
 
 slides[current].classList.add('active');
 
-},3000);
+}, 3000);
+
+
+// CART SYSTEM
+
+let cartCount = 0;
+
+const cartIcon = document.querySelector('.cart-count');
+
+const buttons = document.querySelectorAll('.product-card button');
+
+buttons.forEach(button => {
+
+button.addEventListener('click', () => {
+
+cartCount++;
+
+if(cartIcon){
+cartIcon.innerText = cartCount;
+}
+
+alert('Product Added To Cart');
+
+});
+
+});
