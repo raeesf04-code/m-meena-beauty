122fb75f76d5f4176ae48f7895d323ce7fff7712
raeesf04-code <raeesf04@gmail.com>
2026-06-10 22:46:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
-// HERO SLIDER
-
-let slides = document.querySelectorAll('.slide');
-let current = 0;
-
-setInterval(() => {
-
-slides[current].classList.remove('active');
-
-current++;
-
-if(current >= slides.length){
-current = 0;
-}
-
-slides[current].classList.add('active');
-
-}, 3000);
-
-
-// CART SYSTEM
-
+let cart = [];
+let total = 0;
 let cartCount = 0;
 
-const cartIcon = document.querySelector('.cart-count');
-
 const buttons = document.querySelectorAll('.product-card button');
+const cartItems = document.getElementById('cart-items');
+const totalBox = document.getElementById('cart-total');
+const countBox = document.querySelector('.cart-count');
 
-buttons.forEach(button => {
+buttons.forEach(button=>{
 
-button.addEventListener('click', () => {
+button.addEventListener('click',()=>{
+
+let card = button.parentElement;
+
+let name = card.querySelector('h3').innerText;
+
+let price = parseInt(
+card.querySelector('p').innerText.replace('Rs.','')
+);
+
+cart.push({name,price});
 
 cartCount++;
+countBox.innerText = cartCount;
 
-if(cartIcon){
-cartIcon.innerText = cartCount;
-}
+total += price;
+totalBox.innerText = total;
 
+let item = document.createElement('div');
 
+item.classList.add('cart-item');
+
+item.innerHTML = `
+<span>${name}</span>
+<span>Rs.${price}</span>
+`;
+
+cartItems.appendChild(item);
 
 });
+
+});
+
+document.getElementById('place-order').addEventListener('click',()=>{
+
+let name =
+document.getElementById('name').value;
+
+let phone =
+document.getElementById('phone').value;
+
+let address =
+document.getElementById('address').value;
+
+if(!name || !phone || !address){
+alert('Fill all fields');
+return;
+}
+
+alert('Order Placed Successfully');
 
 });
